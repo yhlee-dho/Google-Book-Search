f7d4a1d34d2ea213380d341da037753dfdfdf215
unknown <yhlee89.dho@gmail.com>
2020-03-27 21:50:45 -0500

    basic file structure

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// static assets for heroku
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// api route
+app.use(routes)
+
+// mongo
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", 
+{ useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`API server is now online on port ${PORT} !!!`);
+});
