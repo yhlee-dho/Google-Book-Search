ebdef1a2a3f8ce07789228e5e27958934601744c
unknown <yhlee89.dho@gmail.com>
2020-03-28 20:18:39 -0500

    new port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // middleware
